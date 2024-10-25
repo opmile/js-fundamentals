72d7d36fdf3e84151e240ddcd75c57aec7935821
Milena Oliveira <milenaoli2703@gmail.com>
2024-10-24 22:01:48 -0300

    commit

---
+++ practiceCondicionals.js
@@ -0,0 +1,11 @@
+
+/*
+if (condition) {
+    // Code to run if condition is true
+  } else {
+    // Code to run if condition is false (optional)
+  }
+*/
+
+let personName = "Milena"
+console.log(personName)
